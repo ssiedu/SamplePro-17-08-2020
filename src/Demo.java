2e5b40ac9b2a96100514101cc15d13b45a3de7d7
manoj <manojs121@rediffmail.com>
2020-08-17 17:24:56 +0530

    17-08

---
+++ src/Demo.java
@@ -0,0 +1,5 @@
+public class Demo {
+    public static void main(String args[]){
+        System.out.println("Hello from main method of Demo class.............!");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+        String s;
+    }
+
+}
+
+//to fix the import (ctrl+shift+I)
+//for auto-complete (ctrl+spacebar)
+//for auto-formatting (alt+shf+F)
