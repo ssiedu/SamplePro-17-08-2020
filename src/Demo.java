d1e2c1b1df4b3e1e904a35e4a5f42cca8272ceb2
manoj <manojs121@rediffmail.com>
2020-08-18 17:29:06 +0530

    18-08

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,16 @@
+
+import banking.branch.AddBranch;
+import banking.emp.AddEmp;
+import banking.emp.perm.CalSalary;
+import banking.emp.temp.CalWages;
+
 public class Demo {
-    public static void main(String args[]){
-        System.out.println("Hello from main method of Demo class.............!");
+    AddEmp ob1;
+    AddBranch ob2;
+    CalWages ob3;
+    CalSalary ob4;
+
+    public static void main(String[] args) {
+        
     }
 }
