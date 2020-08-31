fedeb611e0ffa1528b3c3b3a3f9002e027ab9930
manoj <manojs121@rediffmail.com>
2020-08-31 17:28:51 +0530

    31-08

---
+++ src/OperatorExample.java
@@ -0,0 +1,18 @@
+
+public class OperatorExample {
+
+    public static void main(String[] args) {
+
+        //int op1=10;
+        //int op2=20;
+        //char op1='A';//65
+        //char op2='B';//66
+        //boolean op1=true;
+        //boolean op2=false;
+        String op1="Hello";
+        String op2="User";
+        System.out.println(op1+op2);
+        
+    }
+
+}
